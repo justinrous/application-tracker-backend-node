78af117e6777192e8ab59793b8268dc2d7fbd76d
justinrous <rousj@oregonstate.edu>
2025-09-21 19:47:02 -0700

    JWT is now sent with cookie

--- index.ts
+++ index.ts
@@ -1,12 +1,20 @@
 import express from 'express';
-import 'dotenv/config';
-import { registerUser, getUserPassword } from './models/models.ts';
+import dotenv from 'dotenv';
+// import { registerUser, getUserPassword } from './models/models.ts';
+import * as model from './models/models.ts';
 import cors from 'cors';
 import bcrypt from 'bcrypt';
+import jsonwebtoken from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
 
 const app = express();
 const PORT: number | string = process.env.PORT || 3000;
 
+
+// Initialize Middleware
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 
@@ -15,13 +23,33 @@ async function hashPassword(password: string): Promise<string> {
     return await bcrypt.hash(password, saltRounds);
 }
 
+async function verifyJwt(req: express.Request, res: express.Response, next: express.NextFunction): Promise<any> {
+
+    const jwtSecret = process.env.JWTSECRET;
+    if (!jwtSecret) {
+        console.error('JWT secret not configured');
+        return res.status(500).send('JWT secret not configured');
+    }
+    try {
+        const decoded = jsonwebtoken.verify(req.cookies['token'], jwtSecret);
+        console.log('JWT verified:', decoded);
+        next();
+    }
+    catch (error) {
+        console.error('JWT verification failed:', error);
+        return res.status(401).send('Invalid token');
+    }
+}
+
+
+
 app.post('/api/users/register', async (req, res) => {
     try {
         // Validate user password
         // Check username uniqueness
         // Hash password before saving
         const hashedPassword: string = await hashPassword(req.body.password);
-        await registerUser(req.body.username, hashedPassword);
+        await model.registerUser(req.body.username, hashedPassword);
         res.status(201).send('User registered successfully');
     }
     catch (error) {
@@ -31,7 +59,7 @@ app.post('/api/users/register', async (req, res) => {
 
 app.post('/api/users/login', async (req, res) => {
     // Retreive password by username
-    const userDBHashedPassword: string | null = await getUserPassword(req.body.username);
+    const userDBHashedPassword: string | null = await model.getUserPassword(req.body.username);
     console.log('Retrieved hashed password from DB:', userDBHashedPassword);
 
     if (!userDBHashedPassword) {
@@ -42,11 +70,73 @@ app.post('/api/users/login', async (req, res) => {
     // Compare passwords using bcrypt
     else if (await bcrypt.compare(req.body.password, userDBHashedPassword)) {
         console.log('Login successful');
+        const jwtSecret = process.env.JWTSECRET;
+        if (!jwtSecret) {
+            console.error('JWT secret not configured');
+            res.status(500).send('JWT secret not configured');
+            return;
+        }
+        const token = jsonwebtoken.sign({ username: req.body.username }, jwtSecret, { expiresIn: '2h' });
+        res.cookie('token', token, {
+            httpOnly: true, // helps prevent XSS
+            secure: process.env.NODE_ENV === 'production', // only send cookie over HTTPS in production )
+            sameSite: 'strict', // CSRF protection
+            maxAge: 2 * 60 * 60 * 1000 // 2 hours in ms
+        });
         res.status(200).send('Login successful');
     }
     else {
         console.log('Invalid username or password');
         res.status(401).send('Invalid username or password');
+    }
+});
+
+app.post('/api/applications', verifyJwt, async (req, res) => {
+
+    /* Example request body:
+    /
+
+    type Category = {
+    name: string;
+    id: string;
+}
+
+type Application = {
+    jobTitle: string;
+    companyName: string;
+    applicationDate: string;
+    status: string;
+    categoryName: string;
+}; */
+
+    try {
+        // Check if Category Exists
+        const { jobTitle, companyName, applicationDate, status, categoryName } = req.body;
+        const category = await model.getCategory(categoryName);
+        // If Category does not exist, Create New Category
+        if (!category) {
+            await model.addCategory(categoryName);
+        }
+        // Add application with the new Category
+        await model.addApplication(jobTitle, companyName, categoryName, applicationDate, status);
+
+        // Add new application
+        res.status(201).send('Application added successfully');
+    }
+    catch (error) {
+        console.log('Error adding application:', error);
+        res.status(500).send('Error adding application');
+        return;
+    }
+});
+
+app.get('/api/dashboard', verifyJwt, async (req, res) => {
+    try {
+        const applications = await model.getApplications();
+        res.status(200).json(applications);
+    }
+    catch (error) {
+        res.status(500).send('Error retrieving applications');
     }
 });
 
--- models/models.ts
+++ models/models.ts
@@ -1,13 +1,16 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/AppTrackerDB');
+    await mongoose.connect(process.env.ConnectionString || 'mongodb://localhost:27017/mydatabase');
     console.log('Connected to MongoDB');
 }
 catch (error) {
     console.error('Error connecting to MongoDB:', error);
 }
 
+// User Table Queries
 // Define Schema and Model
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
@@ -28,7 +31,84 @@ async function registerUser(username: string, password: string): Promise<void> {
             throw err;
         });
 }
+/************************************************************
+************* Categories Table Queries **********************
+************************************************************/
+const categorySchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true }
+});
+const Category = mongoose.model('Category', categorySchema);
+
+async function addCategory(name: string): Promise<void> {
+    const newCategory = new Category({ name });
+    newCategory.save()
+        .then(() => console.log('Category added successfully'))
+        .catch(err => {
+            console.error('Error adding category:', err);
+            throw err;
+        });
+}
+
+async function removeCategory(name: string): Promise<void> {
+    Category.deleteOne({ name })
+        .then((result) => {
+            if (result.deletedCount === 0) {
+                console.log('No category found to delete');
+                throw new Error('Category not found');
+            } else {
+                console.log('Category removed successfully');
+            }
+        })
+        .catch(err => {
+            console.error('Error removing category:', err);
+            throw err;
+        });
+}
+
+async function getCategory(name: string): Promise<string | null> {
+    try {
+        return await Category.findOne({ name: name });
+    }
+    catch (error) {
+        console.error('Error retrieving category:', error);
+        throw error;
+    }
+}
+
+async function getCategories(): Promise<string[]> {
+    return Category.find().then(categories => categories.map(cat => cat.name));
+}
 
 
-export { registerUser, getUserPassword };
+/****************************************************************
+ *  Applications Table Queries
+****************************************************************/
+const applicationSchema = new mongoose.Schema({
+    jobTitle: { type: String, required: true },
+    companyName: { type: String, required: true },
+    categoryName: { type: String, required: true },
+    applicationDate: { type: String, required: true },
+    status: { type: String, required: true },
+})
+
+const Application = mongoose.model('Application', applicationSchema);
+
+async function addApplication(jobTitle: string, companyName: string, categoryName: string, applicationDate: string, status: string): Promise<void> {
+    const newApplication = new Application({ jobTitle, companyName, categoryName, applicationDate, status, categoryName });
+    newApplication.save()
+        .then(() => console.log('Application added successfully'))
+        .catch(err => {
+            console.error('Error adding application:', err);
+            throw err;
+        });
+}
+
+async function deleteApplication(id: string): Promise<void> {
+}
+
+async function getApplications(): Promise<any[]> {
+    return Application.find().then(applications => applications);
+}
+
+export { registerUser, getUserPassword, addCategory, removeCategory, getCategory, getCategories, addApplication, deleteApplication, getApplications };
 
